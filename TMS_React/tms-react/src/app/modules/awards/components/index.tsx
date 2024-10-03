c34ba6bae60300817e5685ac9cd61c4ff5659855
VirajRathod17 <nextgenviraj@gmail.com>
2024-10-03 19:50:32 +0530

    changes

--- TMS_React/tms-react/src/app/modules/awards/components/index.tsx
+++ TMS_React/tms-react/src/app/modules/awards/components/index.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
+import DataTable from 'react-data-table-component';
 import Breadcrumb from '../../include/breadcrumbs';
+import Pagination from './pagination';
 
 interface Award {
   id: number;
@@ -13,18 +15,21 @@ interface Award {
   award_date: string;
 }
 
-function Index() {
-
+const Index: React.FC = () => {
   const [awards, setAwards] = useState<Award[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [selectedAwards, setSelectedAwards] = useState<number[]>([]);
   const [isSelectAll, setIsSelectAll] = useState(false);
-  const pageTitle = 'Manage Award'; // Dynamic page title
-    const module = 'award';
-    const moduleTitle = 'Award';
-    useEffect(() => {
-        document.title = pageTitle; 
-    }, [pageTitle]); 
+  const [paginatedAwards, setPaginatedAwards] = useState<Award[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [totalPages, setTotalPages] = useState(1);
+  const pageTitle = 'Manage Award';
+  const moduleTitle = 'Award';
+
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
 
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, checked } = e.target;
@@ -48,11 +53,11 @@ function Index() {
   };
 
   useEffect(() => {
-    const fetchAward = async () => {
+    const fetchAwards = async () => {
       try {
         const token = localStorage.getItem('jwt_token');
         const response = await axios.get(
-          process.env.REACT_APP_API_BASE_URL + 'awards',
+          `${process.env.REACT_APP_API_BASE_URL}awards`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -61,14 +66,30 @@ function Index() {
         );
         setAwards(response.data.data);
       } catch (error) {
-        console.error('Error fetching Award:', error);
+        console.error('Error fetching Awards:', error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchAward();
+    fetchAwards();
   }, []);
+
+  useEffect(() => {
+    const paginateAwards = () => {
+      setLoading(true);
+
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      // const paginatedData = awards.slice(startIndex, startIndex + itemsPerPage);
+      const paginatedData = awards.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+      setPaginatedAwards(paginatedData);
+      setTotalPages(Math.ceil(awards.length / itemsPerPage));
+      setLoading(false);
+    };
+
+    paginateAwards();
+  }, [currentPage, itemsPerPage, awards]);
 
   const deleteAward = (id: number) => {
     const token = localStorage.getItem('jwt_token');
@@ -84,7 +105,7 @@ function Index() {
     }).then((result) => {
       if (result.isConfirmed) {
         axios
-          .delete(process.env.REACT_APP_API_BASE_URL + `awards/${id}`, {
+          .delete(`${process.env.REACT_APP_API_BASE_URL}awards/${id}`, {
             headers: {
               Authorization: `Bearer ${token}`,
             },
@@ -146,93 +167,166 @@ function Index() {
 
   const breadcrumbs = [{ label: 'Manage Award', url: '' }];
 
+  const columns = [
+    {
+      name: <input className="form-check-input" type="checkbox" checked={isSelectAll} onChange={handleSelectAllChange} />,
+      cell: (row: Award) => (
+        <div className="form-check form-check-sm form-check-custom form-check-solid me-3">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value={row.id.toString()}
+            checked={selectedAwards.includes(row.id)}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+      ),
+      sortable: false,
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: false,
+      width: '80px',
+    },
+    {
+      name: 'ID',
+      selector: (row: Award) => row.id,
+      sortable: true,
+      width: '80px',
+    },
+    {
+      name: 'Name',
+      selector: (row: Award) => row.name,
+      sortable: true,
+      width: '200px',
+    },
+    {
+      name: 'Award Year',
+      selector: (row: Award) => row.year,
+      sortable: true,
+      width: '200px',
+    },
+    {
+      name: 'Location',
+      selector: (row: Award) => row.location,
+      sortable: true,
+      width: '250px',
+    },
+    {
+      name: 'Award Date',
+      selector: (row: Award) => row.award_date,
+      sortable: true,
+      width: '150px',
+    },
+    {
+      name: 'Action',
+      cell: (row: Award) => (
+        <>
+        <div className='justify-content-end'>
+          <Link to={`/awards/edit/${row.id}`} className="btn btn-sm btn-light me-2">
+            Edit
+          </Link>
+          <button onClick={() => deleteAward(row.id)} className="btn btn-sm btn-light">
+            Delete
+          </button>
+          </div>
+        </>
+      ),
+    },
+  ];
+
+  const customStyles = {
+    rows: {
+      style: {
+        minHeight: '72px',
+        borderBottom: '1px solid #dee2e6',
+      },
+    },
+    headCells: {
+      style: {
+        paddingLeft: '16px',
+        paddingRight: '16px',
+        fontWeight: 'bold',
+        fontSize: '1rem',
+        color: '#495057',
+        backgroundColor: '#f8f9fa',
+      },
+    },
+    cells: {
+      style: {
+        paddingLeft: '16px',
+        paddingRight: '16px',
+        fontSize: '0.875rem',
+        color: '#212529',
+      },
+    },
+  };
+
   return (
     <>
-    <div className="app-main flex-column flex-row-fluid" id="kt_app_main">
-      <Helmet>
-        <title>{pageTitle ? pageTitle : ''}</title>
-      </Helmet>
-    <div className="d-flex flex-column flex-column-fluid">
-      <div id="kt_app_toolbar" className="app-toolbar">
-        {/* <div id="kt_app_toolbar_container" className="app-container"> */}
-          <Breadcrumb breadcrumbs={breadcrumbs} />
-        {/* </div> */}
-      </div>
-      <div id="kt_app_content" className="app-content flex-column-fluid">
-        <div id="kt_app_content_container" className="app-container">
-          <div className="card card-flush mb-5">
-            <div className="card-body pt-6">
-              {/* <SearchForm /> */}
-            </div>
+      <div className="app-main flex-column flex-row-fluid" id="kt_app_main">
+        <Helmet>
+          <title>{pageTitle}</title>
+        </Helmet>
+        <div className="d-flex flex-column flex-column-fluid">
+          <div id="kt_app_toolbar" className="app-toolbar">
+            <Breadcrumb breadcrumbs={breadcrumbs} />
           </div>
-          <div className="card card-flush mb-5">
-            <div className="card-body pt-5">
-              <div className="d-flex flex-stack mb-5">
-                <div className="d-flex align-items-center position-relative my-1">
-                  <h2 className="mb-0">Home</h2>
+          <div id="kt_app_content" className="app-content flex-column-fluid">
+            <div id="kt_app_content_container" className="app-container">
+              <div className="card card-flush mb-5">
+                <div className="card-body pt-6">
+                  {/* <SearchForm /> */}
                 </div>
-                {selectedAwards.length === 0 ? (
-                    <Link to="/awards/create" className="btn btn-primary" style={{ marginLeft: '10px' }}>
-                        Add
-                    </Link>
-                ) : (
-                    <div className="d-flex justify-content-end align-items-center" data-kt-docs-table-toolbar="selected">
+              </div>
+              <div className="card card-flush mb-5">
+                <div className="card-body pt-5">
+                  <div className="d-flex justify-content-between align-items-center mb-5">
+                    <h2 className="mb-0">{pageTitle}</h2>
+                    {selectedAwards.length === 0 ? (
+                      <Link to="/awards/create" className="btn btn-primary">
+                        Add {moduleTitle}
+                      </Link>
+                    ) : (
+                      <div className="d-flex justify-content-end align-items-center">
                         <div className="fw-bold me-5">
-                            <span className="me-2">{selectedAwards.length}</span> Selected
+                          <span className="me-2">{selectedAwards.length}</span> Selected
                         </div>
                         <button type="button" className="btn btn-primary" onClick={deleteMultiple}>
-                            Remove Selected
+                          Remove Selected
                         </button>
-                    </div>
-                )}
-              </div>
-              <table id="kt_datatable_example_1" className="table align-middle table-row-dashed fs-6 gy-5">
-                <thead>
-                  <tr className="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                    <th className="w-10px pe-2">
-                      <div className="form-check form-check-sm form-check-custom form-check-solid me-3">
-                        <input className="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_datatable_example_1 .form-check-input" checked={isSelectAll} onChange={handleSelectAllChange}/>
                       </div>
-                    </th>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Award Year</th>
-                    <th>Location</th>
-                    <th>Award Date</th>
-                    <th className="text-end min-w-100px">Action</th>
-                  </tr>
-                </thead>
-                <tbody className="text-gray-600 fw-semibold">
-                  {(
-                    awards.map((award) => (
-                      <tr key={award.id}>
-                        <td>
-                          <div className="form-check form-check-sm form-check-custom form-check-solid me-3">
-                            <input className="form-check-input" type="checkbox" value={award.id.toString()} checked={selectedAwards.includes(award.id)} onChange={handleCheckboxChange} />
-                          </div>
-                        </td>
-                        <td>{award.id}</td>
-                        <td>{award.name}</td>
-                        <td>{award.year}</td>
-                        <td>{award.location}</td>
-                        <td>{award.award_date}</td>
-                        <td className="text-end">
-                          <Link to={`/awards/edit/${award.id}`} className="btn btn-sm btn-light me-2">Edit</Link>
-                          <Link to="#" className="btn btn-sm btn-light" onClick={() => deleteAward(award.id)}>Delete</Link>
-                        </td>
-                      </tr>
-                    ))
+                    )}
+                  </div>
+                  {loading ? (
+                    <p>Loading...</p>
+                  ) : (
+                    <DataTable
+                      columns={columns}
+                      data={awards}
+                      customStyles={customStyles}
+                      pagination={false}
+                      striped
+                      highlightOnHover
+                      pointerOnHover
+                      persistTableHead
+                    />
                   )}
-                </tbody>
-              </table>
+                  <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={setCurrentPage}
+                    onItemsPerPageChange={setItemsPerPage}
+                    itemsPerPage={itemsPerPage}
+                    isLoading={loading}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
     </>
   );
-}
+};
 
 export default Index;
